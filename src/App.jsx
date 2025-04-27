c758a879de7bd6036353637e1c4a0cb720816ed5
kxGeou <grzegorzm.work@gmail.com>
2025-04-27 23:51:56 +0200

    localStorage handle
    I coded functions that add and remove days of habit that user added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+
 export default function App() {
+  
+  const [habitDay, setHabitDay] = useState(() => {
+    const saved = localStorage.getItem("habitDays");
+    return saved ? Number(saved) : 0;  
+  });
+  
+  function complete() {
+    setHabitDay(prevHabitDay => {
+      const newHabitDay = prevHabitDay + 1;
+      localStorage.setItem("habitDays", newHabitDay);
+      return newHabitDay;
+    });
+
+    console.log(habitDay)
+  }
+
+  function remove() {
+    localStorage.clear()
+    setHabitDay(0)
+    console.log(habitDay)
+  }
+
   return (
-    <></>
+    <main className="flex flex-col gap-6 justify-center items-center h-screen">
+        <p className={`${habitDay >= 10 ? "bg-red-500" : ""}`}>{habitDay}</p>
+        <button onClick={complete} className="bg-gray-400 cursor-pointer text-black p-2 rounded">Zrobione</button>
+        <button onClick={remove} className="bg-red-600 cursor-pointer text-white p-2 rounded">Zrobione</button>
+    </main>
   );
 }
