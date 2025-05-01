1cf2c7830ee641b684e921d98d2fed002e64e69f
kxGeou <grzegorzm.work@gmail.com>
2025-05-01 23:23:03 +0200

    w

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Habit from "./components/Habit";
-import Timer from "./components/Timer";
 
 export default function App() {
 
@@ -7,7 +6,6 @@ export default function App() {
   return (
     <main className="flex flex-col gap-6 justify-center items-center h-screen">
         <Habit></Habit>
-        <Timer></Timer>
     </main>
   );
 }
