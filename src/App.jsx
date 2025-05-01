5d47c87e35a7a4fc20dc5156b147cbe163ecc5a3
kxGeou <grzegorzm.work@gmail.com>
2025-05-01 12:11:10 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,13 @@
-import { useEffect, useState } from "react";
 import Habit from "./components/Habit";
+import Timer from "./components/Timer";
 
 export default function App() {
-  
-  const [habitDay, setHabitDay] = useState(() => {
-    const saved = localStorage.getItem("habitDays");
-    return saved ? Number(saved) : 0;  
-  });
-  const lastUpdate = localStorage.getItem('lastUpdate');
-  
-  function complete() {
-    setHabitDay(prevHabitDay => {
-      const newHabitDay = prevHabitDay + 1;
-      localStorage.setItem("habitDays", newHabitDay);
-      localStorage.setItem('lastUpdate', Date.now());
-      return newHabitDay;
-    });
-    console.log(habitDay)
-    console.log(lastUpdate)
-  }
-
-  function remove() {
-    localStorage.clear()
-    setHabitDay(0)
-    console.log(habitDay)
-  }
-
-  useEffect(() => {
-      const interval = setInterval(() => {
-      if (lastUpdate) {
-        const now = Date.now();
-        const diffInMs = now - Number(lastUpdate);
-        const diffInHours = diffInMs / (1000 * 60 * 60); 
-      
-        if (diffInHours > 24) {
-          setHabitDay(0);
-          localStorage.setItem('lastUpdate', Date.now());
-          console.log("od nowa")
-        }
-      }
-    }, 5000);
-    return () => clearInterval(interval)
-  } , [])
-
-  
 
 
   return (
     <main className="flex flex-col gap-6 justify-center items-center h-screen">
-        <Habit handleCompltedDay={complete} dayStreak={habitDay} localClear={remove} habitName="Mycie zębów"></Habit>
+        <Habit></Habit>
+        <Timer></Timer>
     </main>
   );
 }
--- src/components/Habit.jsx
+++ src/components/Habit.jsx
@@ -1,13 +1,59 @@
 import Flame from '../assets/flameStreak.png';
+import { useEffect, useState } from "react";
 
-export default function Habit({handleCompletedDay, localClear, dayStreak, habitName}) {
+export default function Habit() {
+     
+  const [habitDay, setHabitDay] = useState(() => {
+    const saved = localStorage.getItem("habitDays");
+    return saved ? Number(saved) : 0;  
+  });
+  const lastUpdate = localStorage.getItem('lastUpdate');
+  
+  function complete() {
+    setHabitDay(prevHabitDay => {
+      const newHabitDay = prevHabitDay + 1;
+      localStorage.setItem("habitDays", newHabitDay);
+      localStorage.setItem('lastUpdate', Date.now());
+      return newHabitDay;
+    });
+    console.log(habitDay)
+    console.log(lastUpdate)
+  }
+
+  function remove() {
+    localStorage.clear()
+    setHabitDay(0)
+    console.log(habitDay)
+  }
+
+  useEffect(() => {
+      const interval = setInterval(() => {
+      if (lastUpdate) {
+        const now = Date.now();
+        const diffInMs = now - Number(lastUpdate);
+        const diffInHours = diffInMs / (1000 * 60 * 60); 
+      
+        if (diffInMs > 1) {
+          setHabitDay(0);
+          localStorage.setItem('lastUpdate', Date.now());
+          console.log("od nowa")
+        }
+      }
+    }, 5000);
+    return () => clearInterval(interval)
+  } , [])
+
+  
+
+   
+   
     return (
         <div>
-            <p>{habitName}</p>
-            <p>{dayStreak}</p>
+            <p>QWE</p>
+            <p>{habitDay}</p>
             <img src={Flame} alt="Flame vector for streak in habit" className='w-[5rem]'/>
-            <button className='bg-white text-black' onClick={() =>  handleCompletedDay}>Zrobione</button>
-            <button className='bg-white text-black' onClick={() =>  localClear}>Nie Zrobione</button>
+            <button className='bg-white text-black' onClick={complete}>Zrobione</button>
+            <button className='bg-white text-black' onClick={remove}>Nie Zrobione</button>
         </div>
     )
 }
